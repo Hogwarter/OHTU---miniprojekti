12de93d07ecd6d8a3f08f682f761d88350262195
pronom8 <mangros19@gmail.com>
2024-12-02 21:52:19 +0200

    todo legacy code poistettu db_helper.py:stä

--- src/db_helper.py
+++ src/db_helper.py
@@ -1,7 +1,7 @@
 from config import db, app
 from sqlalchemy import text
 
-table_name = "todo"
+
 
 def table_exists(name):
   sql_table_existence = text(
@@ -61,26 +61,9 @@ def reset_db():
         """))
         db.session.commit()
 
-def setup_db():
-  if table_exists(table_name):
-    print(f"Table {table_name} exists, dropping")
-    sql = text(f"DROP TABLE {table_name}")
-    db.session.execute(sql)
-    db.session.commit()
 
-  print(f"Creating table {table_name}")
-  sql = text(
-    f"CREATE TABLE {table_name} ("
-    "  id SERIAL PRIMARY KEY, "
-    "  content TEXT NOT NULL,"
-    "  done BOOLEAN DEFAULT FALSE"
-    ")"
-  )
-
-  db.session.execute(sql)
-  db.session.commit()
 
 if __name__ == "__main__":
     with app.app_context():
       reset_db()
-      setup_db()
+      
